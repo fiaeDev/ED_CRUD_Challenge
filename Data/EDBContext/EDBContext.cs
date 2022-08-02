28d2878055cecf2b4ea58046aca902de41a5de30
Sascha <sascha.brauns@tredition.de>
2022-08-02 20:26:57 +0200

    Projektdateien hinzufügen.

---
+++ Data/EDBContext/EDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EDBContext
+{
+    public class EDBContext : DbContext
+    {
+
+        public EDBContext(DbContextOptions<EDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=ED.db");
+        }
+
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DeviceType> DeviceTypes { get; set; }
+        public DbSet<DeviceHealth> DeviceHealths { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {            
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.Property(p => p.Type)
+                    .IsRequired();
+                entity.Property(p => p.Health)
+                    .IsRequired();             
+            });
+
+            modelBuilder.Entity<DeviceType>();
+
+            modelBuilder.Entity<DeviceHealth>();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Data/Entities/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    [Table("Devices")]
+    public class Device
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public int TypeId { get; set; }
+        [ForeignKey("TypeId")]
+        public virtual DeviceType Type { get; set; }        
+        public int HelthId { get; set; }
+        [ForeignKey("HelthId")]
+        public virtual DeviceHealth Health { get; set; }       
+        public DateTime LastUsed { get; set; }
+        public decimal Price { get; set; }
+        public string ColorHexValue { get; set; }
+    }
+}
